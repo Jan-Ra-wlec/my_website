20d8d6e43cf5051b24308753d54d3dd1ecc4f444
Jan-Ra-wlec <jannis.radke@the-white-label.com>
2016-08-30 16:42:55 +0200

    Added primitive Chat with Node JS
    Needs Nodejs

---
+++ nchat/server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+,   app = express()
+,   server = require('http').createServer(app)
+,   io = require('socket.io').listen(server)
+,   conf = require('./config.json');
+
+// Webserver
+// auf den Port x schalten
+server.listen(conf.port);
+
+//app.configure(function(){
+	// statische Dateien ausliefern
+	app.use(express.static(__dirname + '/public'));
+//});
+
+// wenn der Pfad / aufgerufen wird
+app.get('/', function (req, res) {
+	// so wird die Datei index.html ausgegeben
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+// Websocket
+io.sockets.on('connection', function (socket) {
+	// der Client ist verbunden
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
+	// wenn ein Benutzer einen Text senden
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
+	});
+});
+
+// Portnummer in die Konsole schreiben
+console.log('Der Server läuft nun unter http://127.0.0.1:' + conf.port + '/');
+console.log('Drücken sie CTRL + C um den Server zu stoppen.');
