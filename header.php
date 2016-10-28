606794f55ad96a064fa833e62cacbdb2a3872d99
DJayKay <jannisradke@gmx.net>
2016-10-28 22:14:19 +0200

    Testfix

--- header.php
+++ header.php
@@ -1,14 +1,11 @@
 ﻿<!DOCTYPE html>
 <html lang="de">
   <head>
-    <meta charset="utf-8"/>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 			<meta charset="utf-8">
-		<script src="socket.io/socket.io.js"></script>
-		<script src="jquery.min.js"></script>
-		<script src="client.js"></script>
+		<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js?ver=1.9.1'></script>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
