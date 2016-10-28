58a632428bc4223fcc0ce66fb97c4ee86cabfe64
DJayKay <jannisradke@gmx.net>
2016-10-28 20:34:19 +0200

    Fixed Sidebar files

--- sidebar/sample.php
+++ sidebar/sample.php
@@ -1,8 +1,4 @@
-<div class="alert alert-danger" role="alert">
-  <button type="button" class="close" data-dismiss="alert">&times;</button>
-  <span class="glyphicon glyphicon-alert" aria-hidden="true"></span>
-  <span class="sr-only">Error:</span>
-<p>Wie bist du hier her gekommen?<p><br>
-GEH! Hier gibts nichts zu sehen!</p>
-</div>
-</div>
+			<p>Dies ist die Seitenspalte.<br>
+			In der Seitenspalte können z.B. Kontaktdaten oder weitere Links angezeigt werden.<br><br>
+			Auch YouTube Videos könnten eingebaut werden.</p>
+			</div>
